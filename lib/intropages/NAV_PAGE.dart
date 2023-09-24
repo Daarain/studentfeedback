4c7016222072d5c6848faeecaa114fee30154b86
Kazi <daarainkazi@gmail.com>
2023-09-24 19:37:26 +0530

    StudentGrievances

---
+++ lib/intropages/NAV_PAGE.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
+import 'package:studentfeedback/AppColors.dart';
+
+import 'package:studentfeedback/dataprovider.dart';
+import 'package:studentfeedback/intropages/About%20us.dart';
+import 'package:studentfeedback/intropages/Homepage.dart';
+import 'package:studentfeedback/intropages/Select_Sem.dart';
+
+class NAV_PAGE extends StatefulWidget {
+  const NAV_PAGE({super.key});
+
+  @override
+  State<NAV_PAGE> createState() => _NAV_PAGEState();
+}
+
+class _NAV_PAGEState extends State<NAV_PAGE> {
+  int selectedindex=0;
+  void nbg(int index){
+    setState(() {
+      selectedindex=index;
+    });}
+  final List<Widget> screens=[
+    Homepage(),
+    Select_Sem(),
+    Aboutus()
+  ];
+  @override
+  Widget build(BuildContext context) {
+    final String year_shift= ModalRoute.of(context)!.settings.arguments as String;
+    final dataprovider1=Provider.of<dataprovider>(context);
+    final username=dataprovider1.Username1;
+    final password=dataprovider1.Password1;
+    final shift=dataprovider1.Shift;
+    final year=dataprovider1.Year;
+    return Scaffold(
+
+  bottomNavigationBar:BottomNavigationBar(
+      selectedItemColor: Colors.blue,
+      backgroundColor: AppColors.darkblue,
+      currentIndex: selectedindex,
+
+      onTap: nbg,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.account_box_rounded),label: "Greivances"),
+        BottomNavigationBarItem(icon: Icon(Icons.info_outline),label: "About us"),
+
+      ],
+    ),
+    backgroundColor: Colors.transparent,
+    body: screens[selectedindex],
+
+    );
+  }
+}
---
+++ lib/intropages/Select_Sem.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:studentfeedback/dataprovider.dart';
+
+
+class Select_Sem extends StatefulWidget {
+  const Select_Sem({super.key});
+
+  @override
+  State<Select_Sem> createState() => _Select_SemState();
+}
+
+class _Select_SemState extends State<Select_Sem> {
+  @override
+  Widget build(BuildContext context) {
+    final String year_shift= ModalRoute.of(context)!.settings.arguments as String;
+    final dataprovider1=Provider.of<dataprovider>(context);
+    final username=dataprovider1.Username1;
+    final password=dataprovider1.Password1;
+    final shift=dataprovider1.Shift;
+    final year=dataprovider1.Year;
+
+
+
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.blue, Colors.white])),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Center(
+              child: Image.asset(
+                'assets/images/Gpmlogo1.png',
+                height: MediaQuery.of(context).size.height * 0.3,
+                scale: 0.5,
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.7,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)),
+                  // border:Border.all(width: 3,color: Colors.black),
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.blueAccent, Colors.black],
+                  )),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.07,
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                    child: Text(
+                      "Select the Term",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: MediaQuery.of(context).size.height * 0.04,
+                          color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.08,
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(color: Colors.white, width: 2),
+                    ),
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            backgroundColor: Colors.black),
+                        onPressed: () {
+
+
+                          switch (year_shift) {
+                            case "FYFS" || "FYSS":
+                              print('switch working');
+                              Navigator.pushNamed(context, 'homepage_1stsem');
+                              break;
+
+                            case "SYFS" || "SYSS":
+                              print("switch working");
+
+                              Navigator.pushNamed(context, 'homepage_3rdsem');
+                              break;
+
+                            case "TYFS" || "TYSS":
+                              Navigator.pushNamed(context, 'homepage_5thsem');
+                              break;
+                          }
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(),
+                              child: Text(
+                                "ODD TERM",
+                                style: TextStyle(
+                                    fontSize:
+                                        MediaQuery.of(context).size.height *
+                                            0.03,
+                                    fontWeight: FontWeight.w300),
+                              ),
+                            ),
+                            Container(
+                              height: MediaQuery.of(context).size.height * 0.05,
+                              width: MediaQuery.of(context).size.width * 0.1,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.white),
+                              child: Icon(
+                                Icons.navigate_next,
+                                color: Colors.black,
+                                size: MediaQuery.of(context).size.height * 0.05,
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.08,
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.08,
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(color: Colors.black, width: 2),
+                    ),
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            backgroundColor: Colors.white),
+                        onPressed: () {
+                          switch (year_shift) {
+                            case "FYFS" || "FYSS":
+                              Navigator.pushNamed(context, 'homepage_2ndsem');
+                              break;
+
+                            case "SYFS" || "SYSS":
+                              Navigator.pushNamed(context, 'homepage_4thsem');
+                              break;
+
+                            case "TYFS" || "TYSS":
+                              Navigator.pushNamed(context, 'homepage_6thsem');
+                              break;
+                          }
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(),
+                              child: Text(
+                                "Even Term",
+                                style: TextStyle(
+                                    fontSize:
+                                        MediaQuery.of(context).size.height *
+                                            0.03,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black),
+                              ),
+                            ),
+                            Container(
+                              height: MediaQuery.of(context).size.height * 0.05,
+                              width: MediaQuery.of(context).size.width * 0.1,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.black),
+                              child: Icon(
+                                Icons.navigate_next,
+                                color: Colors.white,
+                                size: MediaQuery.of(context).size.height * 0.05,
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
